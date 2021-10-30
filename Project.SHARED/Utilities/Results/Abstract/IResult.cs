a67e7878a330edcd98255c91d586634d23ea1241
naciyeceren097 <naciye.ceren097@gmail.com>
2021-10-30 23:23:54 +0300

    Creating a result structure

---
+++ Project.SHARED/Utilities/Results/Abstract/IResult.cs
@@ -0,0 +1,16 @@
+﻿using Project.SHARED.Utilities.Results.ComplexTypes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.SHARED.Utilities.Results.Abstract
+{
+    public interface IResult
+    {
+        public ResultStatus ResultStatus { get; }
+        public string Message { get; }
+        public Exception Exception { get; }
+    }
+}

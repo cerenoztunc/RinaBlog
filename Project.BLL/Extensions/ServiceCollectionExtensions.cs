1e2b417d461f9e4994b7b81323801099d1a4c7f7
naciyeceren097 <naciye.ceren097@gmail.com>
2021-10-31 15:23:33 +0300

    Extension of the IServiceCollection

---
+++ Project.BLL/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Project.BLL.Abstract;
+using Project.BLL.Concrete;
+using Project.DAL.Abstract;
+using Project.DAL.Concrete;
+using Project.DAL.Concrete.EntityFramework.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.BLL.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection LoadMyServices(this IServiceCollection serviceCollection)
+        {
+            serviceCollection.AddDbContext<RinaBlogContext>(); //DbContext'imizi kayıt ettik..
+            serviceCollection.AddScoped<IUnitOfWork, UnitOfWork>(); //Eğer biri senden IUnitOfWork isterse UnitOfWork demiş olduk..Buradaki scoped olarak eklememizin sebebi dbcontext'in de özünde scope olmasıdır..Scoped her request'te nesnesinin tekrar oluşmasını ve bir request içerisinde sadece bir tane nesne kullanılmasını sağlar. 
+            serviceCollection.AddScoped<ICategoryService, CategoryManager>();
+            serviceCollection.AddScoped<IArticleService, ArticleManager>();
+
+            return serviceCollection;
+        }
+    }
+}

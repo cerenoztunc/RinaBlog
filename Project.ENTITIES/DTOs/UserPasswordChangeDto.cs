1c1db0b70b4b98efeaf0663b1fa241540aa8b66d
naciyeceren097 <naciye.ceren097@gmail.com>
2021-11-08 15:37:57 +0300

    Password change operations

---
+++ Project.ENTITIES/DTOs/UserPasswordChangeDto.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.ENTITIES.DTOs
+{
+    public class UserPasswordChangeDto
+    {
+        
+        [DisplayName("Mevcut Şifreniz")]
+        [Required(ErrorMessage = "{0} alanı boş geçilemez!")]
+        [MaxLength(30, ErrorMessage = "{0} {1} karakterden fazla olamaz!")]
+        [MinLength(5, ErrorMessage = "{0} {1} karakterden az olamaz!")]
+        [DataType(DataType.Password)]
+        public string CurrentPassword { get; set; }
+        [DisplayName("Yeni Şifreniz")]
+        [Required(ErrorMessage = "{0} alanı boş geçilemez!")]
+        [MaxLength(30, ErrorMessage = "{0} {1} karakterden fazla olamaz!")]
+        [MinLength(5, ErrorMessage = "{0} {1} karakterden az olamaz!")]
+        [DataType(DataType.Password)]
+        public string NewPassword { get; set; }
+        [DisplayName("Yeni Şifrenizin Tekrarı")]
+        [Required(ErrorMessage = "{0} alanı boş geçilemez!")]
+        [MaxLength(30, ErrorMessage = "{0} {1} karakterden fazla olamaz!")]
+        [MinLength(5, ErrorMessage = "{0} {1} karakterden az olamaz!")]
+        [DataType(DataType.Password)]
+        [Compare("NewPassword",ErrorMessage ="Şifre alanları birbiri ile uyuşmuyor!")]
+        public string RepeatPassword { get; set; }
+    }
+}

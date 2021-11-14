bab3d76afddc27ca704280231cb5661da5d6d852
naciyeceren097 <naciye.ceren097@gmail.com>
2021-11-15 00:00:59 +0300

    Adding Role Assign process

---
+++ Project.UI/Areas/Admin/Models/UserRoleAssignAjaxViewModel.cs
@@ -0,0 +1,15 @@
+﻿using Project.ENTITIES.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Project.UI.Areas.Admin.Models
+{
+    public class UserRoleAssignAjaxViewModel
+    {
+        public UserRoleAssignDto UserRoleAssignDto { get; set; }
+        public string RoleAssignPartial { get; set; } //hata alırsak dönüelecek
+        public UserDto UserDto { get; set; } //işlem başarılı olduğunda dönülecek mesaj için
+    }
+}

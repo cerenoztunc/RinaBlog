4131548c1fddacadc4ee18ae5dafc6b02817c0ca
naciyeceren097 <naciye.ceren097@gmail.com>
2021-11-21 14:54:26 +0300

    Adding GetAsyncV2 and GetAllAsyncV2 methods

--- Project.SHARED/Data/Concrete/EntityFramework/EfEntityRepositoryBase.cs
+++ Project.SHARED/Data/Concrete/EntityFramework/EfEntityRepositoryBase.cs
@@ -59,6 +59,26 @@ namespace Project.SHARED.Data.Concrete.EntityFramework
             return await query.AsNoTracking().ToListAsync();
         }
 
+        public async Task<IList<T>> GetAllAsyncV2(IList<Expression<Func<T, bool>>> predicates, IList<Expression<Func<T, object>>> includeProperties)
+        {
+            IQueryable<T> query = _context.Set<T>();
+            if (predicates != null && predicates.Any())
+            {
+                foreach (var predicate in predicates)
+                {
+                    query = query.Where(predicate);
+                }
+            }
+            if (includeProperties != null && includeProperties.Any())
+            {
+                foreach (var includeProperty in includeProperties)
+                {
+                    query = query.Include(includeProperty);
+                }
+            }
+            return await query.AsNoTracking().ToListAsync();
+        }
+
         public async Task<T> GetAsync(Expression<Func<T, bool>> predicate, params Expression<Func<T, object>>[] includeProperties)
         {
             IQueryable<T> query = _context.Set<T>();
@@ -67,6 +87,26 @@ namespace Project.SHARED.Data.Concrete.EntityFramework
                 query = query.Where(predicate);
             }
             if (includeProperties.Any())
+            {
+                foreach (var includeProperty in includeProperties)
+                {
+                    query = query.Include(includeProperty);
+                }
+            }
+            return await query.AsNoTracking().SingleOrDefaultAsync();
+        }
+
+        public async Task<T> GetAsyncV2(IList<Expression<Func<T, bool>>> predicates, IList<Expression<Func<T, object>>> includeProperties)
+        {
+            IQueryable<T> query = _context.Set<T>();
+            if (predicates != null && predicates.Any())
+            {
+                foreach(var predicate in predicates)
+                {
+                    query = query.Where(predicate);
+                }
+            }
+            if (includeProperties != null && includeProperties.Any())
             {
                 foreach (var includeProperty in includeProperties)
                 {
